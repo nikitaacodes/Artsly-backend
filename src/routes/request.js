62c51eb8b5fd6e0d11ae7850bf1108df9cb5c166
nikitaacodes <nikitaa4822@gmail.com>
2025-08-20 17:30:20 +0530

    request route

---
+++ src/routes/request.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const { userAuth } = require("../middleware/auth");
+const fRequest = require("../models/Request");
+const reqRouter = express.Router();
+reqRouter.get("/sent", userAuth, async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const sentReq = await fRequest
+      .find({
+        sender: userId,
+        //all sent request
+      })
+      .populate("receiver", "name userName emailId profilePic");
+    res.send(sentReq);
+  } catch (err) {
+    res.status(500).send("error" + err.message);
+  }
+});
+
+reqRouter.get("/received", userAuth, async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const receivedReq = await fRequest
+      .find({ receiver: userId, status: "sent" })
+      .populate("sender", "name userName emailId profilePic");
+
+    res.send(receivedReq);
+  } catch (err) {
+    res.status(500).send("error" + err.message);
+  }
+});
+module.exports = reqRouter;
